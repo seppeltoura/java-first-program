115809971fc4cf7d3fc614f17a84875dba9b3db1
seppeltoura <75138593+seppeltoura@users.noreply.github.com>
2020-12-06 17:52:47 +0100

    module 5 rdy

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -11,8 +11,8 @@ public class Finance {
     public final static String MORTGAGE_CALCULATOR = "mortgageCalculator";
 
     public final static Map<String,String> commandsToUsage = Map.of(BEST_LOAN_RATES, "usage: bestLoanRates",
-            SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits seperated by ','>",
-            MORTGAGE_CALCULATOR, "usage: mortgageCalcualtor <loanAmount> <termInYears> <annualRate>");
+            SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
+            MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>");
 
     private static boolean validateCommandArguments(String[] args) {
         switch (args[0]) {
